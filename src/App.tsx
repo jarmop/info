c9c49a2bd983f43361dd2dde0020a82063f2bb78
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2025-03-20 08:36:53 +0200

    Inline blocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 const data = [{
@@ -11,6 +12,10 @@ const data = [{
 }, {
   name: "PHP",
   date: "1995-6-8",
+  description: "-",
+}, {
+  name: "IMDB",
+  date: "1990",
   description: "-",
 }, {
   name: "Internet Explorer",
@@ -30,24 +35,46 @@ function compare(a: Datum, b: Datum) {
   else return 0;
 }
 
-data.toSorted(compare);
+const sortedData = data.toSorted(compare);
 
 function App() {
+  const [activeBox, setActiveBox] = useState("");
+
   return (
     <div className="m-2">
-      <div className="grid">
-        {data.map((d) => (
-          <div key={d.name} className="box border-2 p-2 text-sm">
-            <h3>{`${d.date} ${d.name}`}</h3>
-            <div className="description">
-              <textarea
-                className="resize-none block"
-                readOnly
-                defaultValue={d.description}
-              />
-            </div>
-          </div>
+      <div className="-m-1">
+        {sortedData.map((d) => (
+          <Box
+            key={d.name}
+            d={d}
+            onClick={() =>
+              activeBox === d.name ? setActiveBox("") : setActiveBox(d.name)}
+          />
         ))}
+      </div>
+    </div>
+  );
+}
+
+function Box(
+  { d, onClick }: { d: Datum; onClick: () => void },
+) {
+  return (
+    <div className="box relative text-sm inline-block align-top">
+      <div className="border-2 p-2 m-1 cursor-pointer">
+        <h3 onClick={onClick}>
+          {`${d.date} ${d.name}`}
+        </h3>
+      </div>
+      <div
+        className="description invisible absolute z-10 border-2 p-2 m-1 -mt-2 top-full cursor-pointer w-max h-max min-w-full min-h-full"
+        onClick={onClick}
+      >
+        <textarea
+          className="resize-none field-sizing-content block cursor-pointer focus-visible:outline-0"
+          readOnly
+          defaultValue={d.description}
+        />
       </div>
     </div>
   );
