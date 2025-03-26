6e9f4ab46c1acd14b5ed90efe61da9d2acd225ff
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2025-03-26 08:15:59 +0200

    Select tags

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import { useData, useMode, useVisibleIds } from './store.ts'
+import { useData, useMode, useTags, useVisibleIds } from './store.ts'
 import { Save } from './Save.tsx'
+import uniq from 'lodash/uniq'
 
 export function Header() {
   const {
@@ -9,48 +10,108 @@ export function Header() {
   } = useVisibleIds()
   const { mode, setMode } = useMode()
   const [value, setValue] = useState('')
+  const [tag, setTag] = useState('')
   const { data } = useData()
+  const { selectedTags, selectTag, deselectTag, resetSelectedTags } = useTags()
 
   const suggestions = data.filter((d) => !visibleIds.includes(d.id))
+  const tags = uniq(suggestions.flatMap((d) => d.tags)).filter((tag) =>
+    tag &&
+    !selectedTags.includes(tag)
+  ) as string[]
 
   return (
-    <div className='bg-gray-200 p-1 flex justify-between'>
-      <div className='flex'>
-        <datalist id='names'>
-          {suggestions.map((d) => (
-            <option key={d.id} value={d.id} label={d.name} />
-          ))}
-        </datalist>
-        <input
-          type='text'
-          placeholder='Search'
-          className='bg-white p-1'
-          value={value}
-          onChange={(e) => {
-            if ('data' in e.nativeEvent) {
-              setValue(e.target.value)
-            } else {
-              // Changed by selecting a suggestion
-              addVisibleId(parseInt(e.target.value))
+    <div className='bg-gray-200 p-1'>
+      <div className='flex justify-between'>
+        <div className='flex'>
+          <datalist id='names'>
+            {suggestions.map((d) => (
+              <option key={d.id} value={d.id} label={d.name} />
+            ))}
+          </datalist>
+          <input
+            type='text'
+            placeholder='Search items'
+            className='bg-white p-1'
+            value={value}
+            onChange={(e) => {
+              if ('data' in e.nativeEvent) {
+                setValue(e.target.value)
+              } else {
+                // Changed by selecting a suggestion
+                addVisibleId(parseInt(e.target.value))
+                setValue('')
+              }
+            }}
+            list='names'
+            onKeyUp={(e) => {
+              if (e.key !== 'Enter') return
+
+              const item = data.find((d) => d.name === value)
+
+              if (!item) return
+
+              addVisibleId(item.id)
               setValue('')
-            }
-          }}
-          list='names'
-          onKeyUp={(e) => {
-            if (e.key !== 'Enter') return
+            }}
+          />
+          <datalist id='tags'>
+            {tags.map((tag) => <option key={tag} value={tag} />)}
+          </datalist>
+          <input
+            type='text'
+            placeholder='Search tags'
+            className='bg-white p-1 ml-2'
+            value={tag}
+            onChange={(e) => {
+              if ('data' in e.nativeEvent) {
+                setTag(e.target.value)
+              } else {
+                // Changed by selecting a suggestion
+                selectTag(e.target.value)
+                setTag('')
+              }
+            }}
+            list='tags'
+            onKeyUp={(e) => {
+              if (e.key !== 'Enter') return
 
-            const item = data.find((d) => d.name === value)
+              const item = data.find((d) => d.name === value)
 
-            if (!item) return
+              if (!item) return
 
-            addVisibleId(item.id)
-            setValue('')
-          }}
-        />
-        <Link mode={mode} setMode={setMode} name='timeline' />
-        <Link mode={mode} setMode={setMode} name='flow' />
+              addVisibleId(item.id)
+              setValue('')
+            }}
+          />
+          <Link mode={mode} setMode={setMode} name='timeline' />
+          <Link mode={mode} setMode={setMode} name='flow' />
+        </div>
+        <Save />
       </div>
-      <Save />
+      <div className='mt-1'>
+        {selectedTags.map((tag) => (
+          <button
+            key={tag}
+            type='button'
+            className='px-1 bg-gray-100 hover:bg-gray-300 cursor-pointer mr-2 border-1 border-gray-500'
+            onClick={() => deselectTag(tag)}
+          >
+            {tag}
+          </button>
+        ))}
+        {selectedTags.length > 1 &&
+          (
+            <button
+              key={tag}
+              type='button'
+              className='px-1 hover:bg-gray-300 cursor-pointer'
+              onClick={resetSelectedTags}
+            >
+              Clear tags
+            </button>
+          )}
+      </div>
     </div>
   )
 }
