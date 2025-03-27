5aeab60b823268af7e003981e0d23dd534bac47e
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2025-03-27 08:39:54 +0200

    Clear item selections

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -72,7 +72,7 @@ export function Sidebar() {
               setActiveId(0)
             }}
           >
-            Cancel
+            Close
           </button>
         </div>
         {item.id > 0 && (
