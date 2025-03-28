5a388fcc0401ad19a4dce48db454f67f87ce82ef
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2025-03-28 08:47:50 +0200

    Format year

--- src/helpers.ts
+++ src/helpers.ts
@@ -19,3 +19,32 @@ function compareId(a: Datum, b: Datum) {
 export function sortById(data: Datum[]) {
   return data.toSorted(compareId)
 }
+
+export function formatYear(year: number) {
+  if (year <= -1e9) {
+    return `${year / 1e9} B`
+  } else if (year <= -1e6) {
+    return `${year / 1e6} M`
+  } else if (year <= -1e4) {
+    return `${year / 1e3} K`
+  }
+
+  return year.toString()
+}
+
+export function parseYear(year: string) {
+  const lastChar = year.slice(-1)
+  const num = parseFloat(year.split(' ')[0])
+
+  let parsedYear = num
+  if (lastChar === 'B') {
+    parsedYear = num * 1e9
+  } else if (lastChar === 'M') {
+    parsedYear = num * 1e6
+  } else if (lastChar === 'K') {
+    parsedYear = num * 1e3
+  }
+
+  // Float multiplication leads to precision errors
+  return Math.round(parsedYear)
+}
